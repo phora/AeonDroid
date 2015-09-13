a12393d0285dd40330f87afba7acdc932e7d7464
phora <phora.public@outlook.com>
2015-09-13 13:03:27 -0700

    We don't have data, so remove data sync

--- app/src/main/java/io/github/phora/aeondroid/activities/SettingsActivity.java
+++ app/src/main/java/io/github/phora/aeondroid/activities/SettingsActivity.java
@@ -90,12 +90,6 @@ public class SettingsActivity extends PreferenceActivity {
         getPreferenceScreen().addPreference(fakeHeader);
         addPreferencesFromResource(R.xml.pref_notification);
 
-        // Add 'data and sync' preferences, and a corresponding header.
-        fakeHeader = new PreferenceCategory(this);
-        fakeHeader.setTitle(R.string.pref_header_data_sync);
-        getPreferenceScreen().addPreference(fakeHeader);
-        addPreferencesFromResource(R.xml.pref_data_sync);
-
         // Bind the summaries of EditText/List/Dialog/Ringtone preferences to
         // their values. When their values change, their summaries are updated
         // to reflect the new value, per the Android Design guidelines.
