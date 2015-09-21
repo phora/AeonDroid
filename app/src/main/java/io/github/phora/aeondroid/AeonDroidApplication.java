12defb7612510bc59dc0d99eae5b942259a8d452
phora <phora.public@outlook.com>
2015-09-21 13:45:36 -0700

    Also set the theme on the application level. The reason for this is that the planet icon hour style indicators break when this isn't done.

---
+++ app/src/main/java/io/github/phora/aeondroid/AeonDroidApplication.java
@@ -0,0 +1,28 @@
+package io.github.phora.aeondroid;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by phora on 9/21/15.
+ */
+public class AeonDroidApplication extends Application {
+    @Override
+    public void onCreate() {
+        if (getIsDark()) {
+            setTheme(R.style.AppThemeDark);
+        }
+        else {
+            setTheme(R.style.AppTheme);
+        }
+
+        super.onCreate();
+    }
+
+    private boolean getIsDark() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isdark = preferences.getBoolean("isDark", false);
+        return isdark;
+    }
+}
