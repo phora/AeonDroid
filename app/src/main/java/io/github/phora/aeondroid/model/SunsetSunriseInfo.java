6598a8f876759589520fe264528cc129cb1f42a3
phora <phora.public@outlook.com>
2015-10-03 13:39:14 -0700

    Extend SunsetSunrise info to have the information necessary to directly calculate the hour number for a given datestamp. This will be useful when writing the code to check the alerts and to optimize the checking of the planetary hours.

--- app/src/main/java/io/github/phora/aeondroid/model/SunsetSunriseInfo.java
+++ app/src/main/java/io/github/phora/aeondroid/model/SunsetSunriseInfo.java
@@ -1,5 +1,8 @@
 package io.github.phora.aeondroid.model;
 
+import java.util.Date;
+
+import io.github.phora.aeondroid.calculations.EphemerisUtils;
 import swisseph.SweDate;
 
 /**
@@ -7,15 +10,26 @@ import swisseph.SweDate;
  */
 public class SunsetSunriseInfo {
     private final SweDate calcTime;
+    private final double dayHourLength;
+    private final double nightHourLength;
     private Double sunset;
     private Double sunrise;
     private Double nextSunrise;
+    private int dayOffset;
 
     public SunsetSunriseInfo(Double sunrise, Double sunset, Double nextSunrise, SweDate calcTime) {
         this.sunset = sunset;
         this.sunrise = sunrise;
         this.nextSunrise = nextSunrise;
         this.calcTime = calcTime;
+        this.dayOffset = SweDate.getDayOfWeekNr(sunrise);
+
+        this.dayHourLength = (sunset - sunrise)/12;
+        this.nightHourLength = (nextSunrise - sunset)/12;
+    }
+
+    public int getDayOffset() {
+        return dayOffset;
     }
 
     public Double getSunset() {
@@ -32,5 +46,73 @@ public class SunsetSunriseInfo {
 
     public SweDate getCalcTime() {
         return calcTime;
+    }
+
+    public double getDayHourLength() {
+        return dayHourLength;
+    }
+
+    public double getNightHourLength() {
+        return nightHourLength;
+    }
+
+    public int calculatePlanetHourNum(Date d) {
+        return calculatePlanetHourNum(EphemerisUtils.dateToSweDate(d).getJulDay());
+    }
+
+    public int calculatePlanetHourNum(Double d) {
+        if (sunrise <= d && d < sunset) {
+            return (int)((d - sunrise)/dayHourLength);
+        }
+        else if (sunset <= d && d < nextSunrise) {
+            return (int)((d - sunset)/nightHourLength)+12;
+        }
+        else if (nextSunrise <= d) {
+            return -1;
+        }
+        else {
+            return -2;
+        }
+    }
+
+    public PlanetaryHour calculatePlanetHour(Date d) {
+        return calculatePlanetHour(EphemerisUtils.dateToSweDate(d).getJulDay());
+    }
+
+    public PlanetaryHour calculatePlanetHour(Double d) {
+        double timestamp;
+        PlanetaryHour.HourClass hclass = PlanetaryHour.HourClass.NORMAL;
+        int nHours;
+        boolean isNight = false;
+
+        if (sunrise <= d && d < sunset) {
+            nHours = (int)((d - sunrise)/dayHourLength);
+            timestamp = nHours*dayHourLength+sunrise;
+            if (nHours == 0) {
+                hclass = PlanetaryHour.HourClass.SUNRISE;
+            }
+        }
+        else if (sunset <= d && d < nextSunrise) {
+            nHours = (int)((d - sunset)/nightHourLength);
+            isNight = true;
+            timestamp = (nHours*nightHourLength)+sunset;
+            if (nHours == 0) {
+                hclass = PlanetaryHour.HourClass.SUNSET;
+            }
+        }
+        else {
+            return null;
+        }
+
+        if (isNight) {
+            return new PlanetaryHour(true, hclass,
+                    (nHours+12+PlanetaryHour.WDAYS_TO_POFFSETS[dayOffset]) % 7,
+                    timestamp, nightHourLength);
+        }
+        else {
+            return new PlanetaryHour(false, hclass,
+                    (nHours+PlanetaryHour.WDAYS_TO_POFFSETS[dayOffset]) % 7,
+                    timestamp, dayHourLength);
+        }
     }
 }
