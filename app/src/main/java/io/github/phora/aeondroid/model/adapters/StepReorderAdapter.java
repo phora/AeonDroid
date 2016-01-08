20b59f2cb47184a030ae96cc7a4693eac8720cc5
phora <phora.public@outlook.com>
2016-01-08 14:08:26 -0800

    Remove unused ImageButton import

--- app/src/main/java/io/github/phora/aeondroid/model/adapters/StepReorderAdapter.java
+++ app/src/main/java/io/github/phora/aeondroid/model/adapters/StepReorderAdapter.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
