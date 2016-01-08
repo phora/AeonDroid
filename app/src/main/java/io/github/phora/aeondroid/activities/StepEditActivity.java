874986b913d50edd2cc1729f17bc56683d38424e
phora <phora.public@outlook.com>
2016-01-08 14:13:31 -0800

    Remove unused imports in StepEditActivity

--- app/src/main/java/io/github/phora/aeondroid/activities/StepEditActivity.java
+++ app/src/main/java/io/github/phora/aeondroid/activities/StepEditActivity.java
@@ -1,20 +1,15 @@
 package io.github.phora.aeondroid.activities;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
-import android.provider.MediaStore;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 import io.github.phora.aeondroid.R;
 import yuku.ambilwarna.AmbilWarnaDialog;
-import yuku.ambilwarna.AmbilWarnaSquare;
 
 public class StepEditActivity extends Activity {
 
