acf368f89af94aa4e22a8aa24f833b714a79fb81
phora <phora.public@outlook.com>
2016-01-08 14:11:49 -0800

    Fix image and color saving/loading in steps
    - Images needed a URI with a permission that would allow them to be
      viewed in the app after saving.
    - getSolidColor doesn't return the selected color. So instead, we're
      also setting the picked color as the tag for retrieving in finishEdit.

--- app/src/main/java/io/github/phora/aeondroid/activities/StepEditActivity.java
+++ app/src/main/java/io/github/phora/aeondroid/activities/StepEditActivity.java
@@ -53,26 +53,31 @@ public class StepEditActivity extends Activity {
         mImageView = (ImageView)findViewById(R.id.StepEdit_Img);
         mColorView = findViewById(R.id.StepEdit_ColorView);
 
-        if (getIntent() != null) {
-            stepId = getIntent().getLongExtra(EXTRA_STEP_ID, -1);
-            alertId = getIntent().getLongExtra(EXTRA_ALERT_ID, -1);
+        Intent intent = getIntent();
+        if (intent != null) {
+            stepId = intent.getLongExtra(EXTRA_STEP_ID, -1);
+            alertId = intent.getLongExtra(EXTRA_ALERT_ID, -1);
 
-            mRepEdit.setText(String.valueOf(getIntent().getIntExtra(EXTRA_REPS, 0)));
-            mUrlEdit.setText(getIntent().getStringExtra(EXTRA_URL));
-            mDescEdit.setText(getIntent().getStringExtra(EXTRA_DESC));
-            imageSrc = Uri.parse(getIntent().getStringExtra(EXTRA_IMAGE));
+            mRepEdit.setText(String.valueOf(intent.getIntExtra(EXTRA_REPS, 0)));
+            mUrlEdit.setText(intent.getStringExtra(EXTRA_URL));
+            mDescEdit.setText(intent.getStringExtra(EXTRA_DESC));
+            String uri = intent.getStringExtra(EXTRA_IMAGE);
+            if (uri == null) {
+                imageSrc = Uri.parse("");
+            }
+            else {
+                imageSrc = Uri.parse(uri);
+            }
             mImageView.setImageURI(imageSrc);
-            mColorView.setBackgroundColor(getIntent().getIntExtra(EXTRA_COLOR, 0));
+            int curColor = intent.getIntExtra(EXTRA_COLOR, 0);
+            mColorView.setBackgroundColor(curColor);
+            mColorView.setTag(curColor);
         }
     }
 
     public void changeImage(View view) {
-        Intent requestFilesIntent = new Intent(Intent.ACTION_GET_CONTENT);
-        requestFilesIntent.addCategory(Intent.CATEGORY_OPENABLE);
-        requestFilesIntent.setType("image/*");
-
-        Intent intent = Intent.createChooser(requestFilesIntent, getString(R.string.ChooseImage));
-        startActivityForResult(intent, CHANGED_IMAGE);
+        Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, CHANGED_IMAGE);
     }
 
     public void cancelEdit(View view) {
@@ -100,9 +105,10 @@ public class StepEditActivity extends Activity {
 
         intent.putExtra(EXTRA_REPS, Integer.valueOf(mRepEdit.getText().toString()));
         intent.putExtra(EXTRA_URL, mUrlEdit.getText().toString());
-        intent.putExtra(EXTRA_DESC, mDescEdit.getText());
+        intent.putExtra(EXTRA_DESC, mDescEdit.getText().toString());
         intent.putExtra(EXTRA_IMAGE, imageSrc.toString());
-        intent.putExtra(EXTRA_COLOR, mColorView.getSolidColor());
+
+        intent.putExtra(EXTRA_COLOR, (Integer)mColorView.getTag());
 
         setResult(RESULT_OK, intent);
         finish();
@@ -119,6 +125,7 @@ public class StepEditActivity extends Activity {
                     @Override
                     public void onOk(AmbilWarnaDialog dialog, int color) {
                         mColorView.setBackgroundColor(color);
+                        mColorView.setTag(color);
                     }
                 });
         ambilWarnaDialog.show();
